f6a7f2004aa0ec8924673c187054a7bcba2e4d0d
muklas <76889639+muklasss@users.noreply.github.com>
2021-01-14 22:42:20 +0700

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -29,7 +29,7 @@ I'm group manager bot and Anti-spam for RR Players🃏
 Activate Anti-spam protection in your BM by this command : 
 /antispam on 
 
-Made from the source code of [Muklas 🇮🇳](t.me/@uklassssss)
+Made from the source code of [Muklas 🇮🇳](t.me/@muklassssss)
 
 Special thanks to [this person](t.me/denver02) for helping me.
 """
